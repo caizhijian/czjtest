be795e1554d2fd72fe4b72aff1ca5f149cf2ec1c
caizhijian <931617277@qq.com>
2021-07-20 22:35:12 +0800

    增加mybatisplus的支持

---
+++ src/main/java/com/czj/module/CzjTestAPP.java
@@ -0,0 +1,18 @@
+package com.czj.module;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:caizhijian
+ * @Date:2021-07-20
+ */
+@SpringBootApplication
+@MapperScan("com.czj.demodruid.mapper")
+public class CzjTestAPP {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CzjTestAPP.class,args);
+    }
+}
