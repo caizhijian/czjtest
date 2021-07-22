321d78264028e49d1b4634dba4e1f83ed10cc84a
caizhijian <931617277@qq.com>
2021-07-23 00:33:43 +0800

    代码调整，启用定时任务

--- src/main/java/com/czj/module/CzjTestAPP.java
+++ src/main/java/com/czj/module/CzjTestAPP.java
@@ -3,15 +3,21 @@ package com.czj.module;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @Author:caizhijian
  * @Date:2021-07-20
  */
 @SpringBootApplication
-@MapperScan("com.czj.module")
+@MapperScan("com.czj")
 @EnableAsync
+@EnableScheduling
+@ComponentScan(basePackages = {
+        "com.czj"
+})
 public class CzjTestAPP {
 
     public static void main(String[] args) {
--- src/main/java/com/czj/module/task/SysTenderProjectTask.java
+++ src/main/java/com/czj/module/task/SysTenderProjectTask.java
@@ -1,9 +1,17 @@
 package com.czj.module.task;
 
+import com.czj.module.tender.entity.TenderSysRecord;
 import com.czj.module.tender.service.ITenderProjectService;
+import com.czj.module.tender.service.ITenderSysRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -13,12 +21,32 @@ import java.util.List;
 @Component
 public class SysTenderProjectTask {
 
+    @Autowired
     private ITenderProjectService tenderProjectService;
 
-    @Scheduled(cron = "0 0 23 * * ?")
+    @Autowired
+    private ITenderSysRecordService tenderSysRecordService;
+
+    @Scheduled(cron = "0 */2 * * * ?")
     public void sysTenderProject() throws Exception {
         System.out.println("定时器启动");
-        tenderProjectService.sysTenderProject("2018-01-01","2018-05-31");
+        Date lastRecord = tenderSysRecordService.getLastRecord();
+        Instant instant = lastRecord.toInstant();
+        ZoneId zone = ZoneId.systemDefault();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zone);
+        LocalDate lastDate = localDateTime.toLocalDate();
+        LocalDate start = lastDate.plusDays(1);
+        LocalDate end = lastDate.plusDays(30);
+        tenderProjectService.sysTenderProject(start.toString(),end.toString());
+
+        TenderSysRecord record = new TenderSysRecord();
+        Instant startInstant = start.atStartOfDay().atZone(zone).toInstant();
+        Instant endInstant = start.atStartOfDay().atZone(zone).toInstant();
+
+        record.setStartDate(Date.from(startInstant));
+        record.setEndDate(Date.from(endInstant));
+        tenderSysRecordService.save(record);
+        System.out.println("完成同步");
     }
 
 }
